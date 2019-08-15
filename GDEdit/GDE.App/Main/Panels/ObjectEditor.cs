5fde505af127b7237db5b15853f8cc3db41db9d7
Alten <altenyt@gmail.com>
2019-08-15 10:47:43 -0400

    Initialize panel

---
+++ GDEdit/GDE.App/Main/Panels/ObjectEditor.cs
@@ -0,0 +1,17 @@
+﻿using GDAPI.Utilities.Objects.GeometryDash.LevelObjects;
+using osu.Framework.Bindables;
+
+namespace GDE.App.Main.Panels
+{
+    public class ObjectEditor : Panel
+    {
+        public Bindable<GeneralObject> ObjectBindable;
+
+        public ObjectEditor(GeneralObject Object)
+        {
+            ObjectBindable = new Bindable<GeneralObject>(Object);
+        }
+
+
+    }
+}
