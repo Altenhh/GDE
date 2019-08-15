939ae8970814b134bdbecd42f398c3c7a4f61fb7
Alten <altenyt@gmail.com>
2019-08-15 14:58:58 -0400

    *even more confused screaming*

--- GDEdit/GDE.App/Main/GDEAppBase.cs
+++ GDEdit/GDE.App/Main/GDEAppBase.cs
@@ -43,19 +43,19 @@ namespace GDE.App.Main
 
             //Purista | Default font
             Fonts.AddStore(new GlyphStore(Resources, @"Fonts/Purista/Purista"));
-            //Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-Italic"));
+            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-Italic"));
 
-            //Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-Bold"));
-            //Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-BoldItalic"));
+            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-Bold"));
+            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-BoldItalic"));
 
-//            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-SemiBold"));
-//            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-SemiBoldItalic"));
+            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-SemiBold"));
+            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-SemiBoldItalic"));
 
-//            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-Light"));
-//            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-LightItalic"));
+            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-Light"));
+            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-LightItalic"));
 
-//            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-Thin"));
-//            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-ThinItalic"));
+            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-Thin"));
+            Fonts.AddStore(new GlyphStore(Resources, "Fonts/Purista/Purista-ThinItalic"));
 
             //Noto | To support most Unicodes & Japanese letters
             Fonts.AddStore(new GlyphStore(Resources, "Fonts/Noto/Noto-Basic"));
