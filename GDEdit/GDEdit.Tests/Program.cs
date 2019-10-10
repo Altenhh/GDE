5871447d55143844baa859e248743f033cbd734a
Alten <altenyt@gmail.com>
2019-10-09 20:10:49 -0400

    Create projs and rename Resources
    Signed-off-by: Alten <altenyt@gmail.com>

---
+++ GDEdit/GDEdit.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GDEdit.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
