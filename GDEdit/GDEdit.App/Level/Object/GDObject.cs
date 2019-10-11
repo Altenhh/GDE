b9407fd5730c10511666c76dc16f7212dc06000e
Alten <altenyt@gmail.com>
2019-10-10 23:13:55 -0400

    Add docs and remove ID
    Signed-off-by: Alten <altenyt@gmail.com>

--- GDEdit/GDEdit.App/Level/Object/GDObject.cs
+++ GDEdit/GDEdit.App/Level/Object/GDObject.cs
@@ -6,7 +6,6 @@ namespace GDEdit.App.Level.Object
 {
     public class GDObject : GeneralObject, IGDObject
     {
-        public int ID { get; set; }
         public Vector2 Offset { get; set; } = Vector2.Zero;
 
         public Drawable[] CreateDrawable()
--- GDEdit/GDEdit.App/Level/Object/IGDObject.cs
+++ GDEdit/GDEdit.App/Level/Object/IGDObject.cs
@@ -5,8 +5,10 @@ namespace GDEdit.App.Level.Object
 {
     public interface IGDObject
     {
-        int ID { get; set; }
+        /// <summary>Origin offset.</summary>
         Vector2 Offset { get; set; }
+        /// <summary>Create what you would see on the Level Preview.</summary>
+        /// <returns>The correct interpretation of the object.</returns>
         Drawable[] CreateDrawable();
     }
 }
