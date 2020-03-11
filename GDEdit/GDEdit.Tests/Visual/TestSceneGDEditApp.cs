d54bb24a8979e3ddb8a557d47d0665b9865a3397
Noah <altenyt@gmail.com>
2020-03-11 18:46:27 -0400

    add app testing

---
+++ GDEdit/GDEdit.Tests/Visual/TestSceneGDEditApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using GDEdit.App;
+using osu.Framework.Allocation;
+using osu.Framework.Platform;
+using osu.Framework.Testing;
+
+namespace GDEdit.Tests.Visual
+{
+    public class TestSceneGDEditApp : TestScene
+    {
+        private GDEMain app;
+
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(GDEMain)
+        };
+
+        [BackgroundDependencyLoader]
+        private void load(GameHost host)
+        {
+            app = new GDEMain();
+            app.SetHost(host);
+            
+            Add(app);
+        }
+    }
+}
